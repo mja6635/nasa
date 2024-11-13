70f383b34ec7ee09d7e1fe148b5d263a426228ce
mja6635 <mja6635@psu.edu>
2024-11-13 15:26:24 -0500

    first commit

---
+++ nasa-search.js
@@ -0,0 +1,106 @@
+import { LitElement, html, css } from "lit";
+import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
+import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+import "./nasa-image.js";
+
+/**
+ 
+nasa-search
+@demo index.html
+@element nasa-search*/
+
+export class NasaSearch extends DDDSuper(I18NMixin(LitElement)) {
+
+  static get tag() {
+    return "nasa-search";
+  }
+
+  static get properties() {
+    return {
+      title: { type: String },
+      loading: { type: Boolean, reflect: true },
+      items: { type: Array, },
+      value: { type: String },
+    };
+  }
+
+  constructor() {
+  super();
+  this.title = "";
+  this.loading = false;
+  this.items = [];
+  this.value = null;
+}
+
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        background-color: var(--dd-default-forestGreen);
+      }
+      .wrap {
+        margin: var(--ddd-spacing-4);
+        padding: var(--ddd-spacing-4);
+      }
+      .results {
+        margin: var(--ddd-spacing-4);
+      }
+    `;
+  }
+
+  render() {
+    return html`
+    <h2>${this.title}</h2>
+    <details open>
+      <summary>Search inputs</summary>
+      <div>
+        <input id="input" placeholder="Search NASA images" @input="${this.inputChanged}" />
+      </div>
+    </details>
+    <div class="results">
+      ${this.items.map((item, index) => html`
+      <nasa-image
+        source="${item.links[0].href}"
+        title="${item.data[0].title}"
+        owner="${item.data[0].secondary_creator}"
+      ></nasa-image>
+      `)}
+    </div>
+    `;
+  }
+
+  inputChanged(e) {
+    this.value = this.shadowRoot.querySelector('#input').value;
+  }
+  // life cycle will run when anything defined in `properties` is modified
+  updated(changedProperties) {
+    // see if value changes from user input and is not empty
+    if (changedProperties.has('value') && this.value) {
+      this.updateResults(this.value);
+    }
+    else if (changedProperties.has('value') && !this.value) {
+      this.items = [];
+    }
+    // @debugging purposes only
+    if (changedProperties.has('items') && this.items.length > 0) {
+      console.log(this.items);
+    }
+  }
+
+  updateResults(value) {
+    this.loading = true;
+    fetch(`https://images-api.nasa.gov/search?media_type=image&q=${value}`).then(d => d.ok ? d.json(): {}).then(data => {
+      if (data.collection) {
+        this.items = [];
+        this.items = data.collection.items;
+        this.loading = false;
+      }  
+    });
+  }
+
+  static get tag() {
+    return 'nasa-search'
+  }
+  }
+customElements.define(NasaSearch.tag, NasaSearch);
